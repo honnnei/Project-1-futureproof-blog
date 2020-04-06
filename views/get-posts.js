cb0516fb24f3d2ac5f4d677e164c05da9571a04b
honnnei <hannpop@gmail.com>
2020-04-06 16:58:09 +0200

    display of posts works

--- views/get-posts.js
+++ views/get-posts.js
@@ -2,17 +2,36 @@ $( document ).ready(function() {
 
     axios.get('/get-posts').then(response => {
         console.log(response);
-        displayPosts = response.data.posts[0].blogPost;
-        displayUsername = response.data.posts[0].username;
-    //   $("#text").css({
-    //     "text-align": "center",
-    //     "min-height": "150px",
-    //     "display": "flex",
-    //     "justify-content": "center",
-    //     "align-items": "center"
-    //   });
-    $('#user').html(`${displayUsername} :`)
-    $("#text").html(`${displayPosts}`);
-    
+        // displayPosts = response.data.posts[0].blogPost;
+        // displayUsername = response.data.posts[0].username;
+        let blogArray = response.data.posts;
+        console.log(Array.isArray(blogArray));
+        displayedBlogArray = blogArray.map(element => {
+        console.log(element);
+            let index = blogArray.indexOf(element);
+            let postUser = element.username;
+            let postContent = element.blogPost;
+            console.log(index);
+            console.log(postUser);
+            console.log(postContent);
+            displayPost(index, postUser, postContent);
+        });
+     
+function displayPost(arrayIndex, arrayUser, arrayPost ) {
+    let id = arrayIndex.toString();
+    console.log(id);
+    $("#blogDisplay").append(`<div id=${id}></div>`);
+    $(`#${id}`).append(`<div class="user_name"></div>`);
+    $(`#${id} .user_name`).html(`${arrayUser}`);
+    $(`#${id}`).append(`<div class="user_post"></div>`);
+    $(`#${id} .user_post`).html(`${arrayPost}`);
+    $(`#${id}`).append(`<button class="comment">Comment</button>`);
+
+
+
+}
+
     })
-  })
+
+
+});
