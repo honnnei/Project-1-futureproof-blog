eba3529d07bc65f83205d3411c47c606599c0a36
Rosie Phelan <rosiephelan@hotmail.co.uk>
2020-04-06 13:09:27 +0100

    tests on blog post json

--- views/get-posts.js
+++ views/get-posts.js
@@ -2,8 +2,22 @@ $( document ).ready(function() {
 
     axios.get('/get-posts').then(response => {
         console.log(response);
-        displayPosts = response.data.posts[1].blogPost;
-        displayUsername = response.data.posts[1].username;
+        let postJson = response.data.post;
+        return postJson;
+    })
+    .then(json => {
+        let postArray = JSON.parse(postJson);
+        console.log(postArray);
+    });
+        // let postArray = JSON.parse(postJson);
+        // console.log(postArray);
+        // for (i=0; i < response.data.post.length; i++) {
+        //     displayPosts = response.data.posts[i].blogPost;
+        //     displayUsername = response.data.posts[i].username;
+        //     console.log(displayPosts);
+        // }
+        // displayPosts = response.data.posts[1].blogPost;
+        // displayUsername = response.data.posts[1].username;
     //   $("#text").css({
     //     "text-align": "center",
     //     "min-height": "150px",
@@ -11,7 +25,7 @@ $( document ).ready(function() {
     //     "justify-content": "center",
     //     "align-items": "center"
     //   });
-    $('#user').html(`${displayUsername} :`)
-    $("#text").html(`${displayPosts}`);
-    })
-  })
+    // $('#user').html(`${displayUsername} :`)
+    // // $("#text").html(`${displayPosts}`);
+    // });
+  });
