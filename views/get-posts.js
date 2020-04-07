1d9ff5b9e605e260556daa627378508c6c6f5da0
honnnei <hannpop@gmail.com>
2020-04-07 18:23:28 +0200

    comment display works!

--- views/get-posts.js
+++ views/get-posts.js
@@ -1,92 +1,32 @@
-$( document ).ready(function() {
+$(document).ready(function () {
     axios.get('/get-posts').then(response => {
-        console.log(response);
-        // displayPosts = response.data.posts[0].blogPost;
-        // displayUsername = response.data.posts[0].username;
-        let blogArray = response.data.posts;
+        console.log(response);  
+      let blogArray = response.data.posts;
         console.log(Array.isArray(blogArray));
         displayedBlogArray = blogArray.map(element => {
-        console.log(element);
+            console.log(element);
             let index = blogArray.indexOf(element);
             let postUser = element.username;
             let postContent = element.blogPost;
             let commentsPresent = element.comments[0];
-            
+
             if (!commentsPresent) {
-            
+
                 displayPostNoComment(index, postUser, postContent);
                 // console.log('this is a post with no comment');
             } else {
-                // console.log('this is a post with yes comments');
                 let arrayOfComments = element.comments;
-                let numberOfComments = arrayOfComments.length;
-                displayedComments = arrayOfComments.map(comment => {
-                    let commentIndex = arrayOfComments.indexOf(comment);
-                    let postCommentUserName = element.comments[commentIndex].commentUsername;
-                    let postCommentContent = element.comments[commentIndex].commentContent;
-                    //invoking the display functtion:
-                    displayPostWithComment(index, commentIndex, postUser, postContent, postCommentUserName, postCommentContent, numberOfComments );
-                });   //comment array map close
+                displayPostWithComment(index, postUser, postContent, arrayOfComments, element);
             } // if statement close 
-            
-            
-            }); //array map close
+        }); //array map close
+    }); //axios close
 
-        }); //axios close
-     
-        function displayPostWithComment(arrayIndex, commentIndex, arrayUser, arrayPost, commentUser, commentValue, commentNumber) {
-            let id = arrayIndex.toString();
-            let thisWillBeCommentId = commentIndex.toString();
-            let commentId = id + thisWillBeCommentId;
-            console.log(id);
-            $("#blogDisplay").append(
-                `<div class="post_div" id=${id}>
-                    <div class="post_section">
-                    <div class="user_name"></div>
-                    <div class="user_post"></div>
-                    <button class="comment">Comment</button>
-                    </div>
-                    <div class="comment_section"></div>
-                </div>`);
-            
-            
-            
-            
-            // $(`#${id} .post_section`).append(`<div class="user_name"></div>`);
-            $(`#${id} .post_section .user_name`).html(`${arrayUser}`);
-            // $(`#${id} .post_section`).append(`<div class="user_post"></div>`);
-            $(`#${id} .post_section .user_post`).html(`${arrayPost}`);
-            // $(`#${id} .post_section`).append(`<button class="comment">Comment</button>`);
-
-
-            // $(`#${id} .post_section `).append(`<form action="/comment/${id}" method="POST" class="comment-container"><label for = "usernamebox">Username</label><input name="commentUsername" type="text" class="usernamebox"/><label for = "commentbox">Comment here</label><input name="commentContent" type="textarea" class="commentbox"/><button type = "submit">Submit</button></form>`);
-
-            // $(`#${id} .post_section .comment`).on('click', function() {
-            //     $(`#${id} .post_section form`).css("display", "flex");
-            // });
-                
-                if (commentIndex === 0) { 
-                $(`#${id}`).append(`<div class="comment_section"></div>`);
-                $(`#${id} .comment_section`).append(`<div id=${commentId} class="comment_container"></div>`);
-                $(`#${commentId}`).append(`<div class="comment_username"></div>`);
-                $(`#${commentId} .comment_username`).html(`${commentUser}`);
-                $(`#${commentId}`).append(`<div class="comment_content"></div>`);
-                $(`#${commentId} .comment_content`).html(`${commentValue}`);
-            } else {
-                $(`#${id} .comment_section`).append(`<div id=${commentId} class="comment_container"></div>`);
-                $(`#${commentId}`).append(`<div class="comment_username"></div>`);
-                $(`#${commentId} .comment_username`).html(`${commentUser}`);
-                $(`#${commentId}`).append(`<div class="comment_content"></div>`);
-                $(`#${commentId} .comment_content`).html(`${commentValue}`);
-            }
-        }
-
-        function displayPostNoComment(arrayIndex, arrayUser, arrayPost) {
-            let id = arrayIndex.toString();
-            console.log(id);
-
-            $("#blogDisplay").append(
-                `<div class="post_div" id=${id}>
+    function displayPostWithComment(arrayIndex, arrayUser, arrayPost, commentArray, postElement ) {
+        //taken out of the function arguments: commentIndex, commentUser, commentValue, commentNumber
+        let id = arrayIndex.toString();
+        //displaying the post:
+        $("#blogDisplay").append(
+            `<div class="post_div" id=${id}>
                     <div class="post_section">
                         <div class="user_name"></div>
                         <div class="user_post"></div>
@@ -103,64 +43,65 @@ $( document ).ready(function() {
                     </div>
                     <div class="comment_section"></div>
                 </div>`);
-              
-            
-            // $(`#${id} .post_section`).append(`<div class="user_name"></div>`);
-            $(`#${id} .post_section .user_name`).html(`${arrayUser}`);
-            // $(`#${id} .post_section`).append(`<div class="user_post"></div>`);
-            $(`#${id} .post_section .user_post`).html(`${arrayPost}`);
-            // $(`#${id} .post_section`).append(`<button class="comment">Comment</button>`);
+        $(`#${id} .post_section .user_name`).html(`${arrayUser}`);
+        $(`#${id} .post_section .user_post`).html(`${arrayPost}`);
+        $(`#${id} .comment`).on('click', function () {
+            $(`#${id} .form_div`).toggle();
+        });
+        //mapping comments:
+        displayedComments = commentArray.map(comment => {
+                let commentIndex = commentArray.indexOf(comment);
+                let postCommentUserName = postElement.comments[commentIndex].commentUsername;
+                let postCommentContent = postElement.comments[commentIndex].commentContent;
+                //invoking the display function:
+                writeComment(id, commentIndex, postCommentUserName, postCommentContent);        
+        }); 
+    }
 
-            $(`#${id} .comment`).on('click', function() {
-                    $(`#${id} .form_div`).toggle();
-            });
-                    
+    function writeComment(idOfPost, indexOfComment, userOfComment, contentOfComment) {
 
-            // function() {
-            //     $(`#${id} .form_div`).css("display", "flex");
-            // }, function() {
-            //     $(`#${id} .form_div`).css("display", "none");
-            // });
-                    // addClass(".form_div_show");
-                    // console.log('button was clickerd');
-            
+        let thisWillBeCommentId = indexOfComment.toString();
+        let commentId = idOfPost + thisWillBeCommentId;
 
-                // $('#user_button').toggle(function () {
-                //     $("#user_button").css({borderBottomLeftRadius: "0px"});
-                // }, function () {
-                //     $("#user_button").css({borderBottomLeftRadius: "5px"});
-                // });
+        $(`#${idOfPost} .comment_section`).append(
+            `<div id=${commentId} class="comment_container">
+                <div class="comment_username"></div>
+                <div class="comment_content"></div>
+            </div>`
+        );
+        $(`#${commentId} .comment_username`).html(`${userOfComment}`);    
+        $(`#${commentId} .comment_content`).html(`${contentOfComment}`);   
+    }
 
+    function displayPostNoComment(arrayIndex, arrayUser, arrayPost) {
+        let id = arrayIndex.toString();
+        console.log(id);
 
-            // $("#blogDisplay").append(
-            //     `<div class="post_div" id=${id}>
-            //         <div class="post_section"></div>
-            //         <div class="comment_section"></div>
-            //     </div>`);
-            // $(`#${id} .post_section`).append(`<div class="user_name"></div>`);
-            // $(`#${id} .post_section .user_name`).html(`${arrayUser}`);
-            // $(`#${id} .post_section`).append(`<div class="user_post"></div>`);
-            // $(`#${id} .post_section .user_post`).html(`${arrayPost}`);
-            // $(`#${id} .post_section`).append(`<button class="comment">Comment</button>`);
+        $("#blogDisplay").append(
+            `<div class="post_div" id=${id}>
+                    <div class="post_section">
+                        <div class="user_name"></div>
+                        <div class="user_post"></div>
+                        <button class="comment">Comment</button>
+                        <div class="form_div">
+                            <form action="/comment/${id}" method="POST" class="comment-container">
+                                <label for = "usernamebox">Username</label>
+                                <input name="commentUsername" type="text" class="usernamebox"/>
+                                <label for = "commentbox">Comment here</label>
+                                <input name="commentContent" type="textarea" class="commentbox"/>
+                                <button type = "submit">Submit</button>
+                            </form>
+                        </div>
+                    </div>
+                    <div class="comment_section"></div>
+                </div>`);
+        $(`#${id} .post_section .user_name`).html(`${arrayUser}`);
+        $(`#${id} .post_section .user_post`).html(`${arrayPost}`);
+        $(`#${id} .comment`).on('click', function () {
+            $(`#${id} .form_div`).toggle();
+        });
 
-
-            // $(`#${id} .post_section`).append(`<div class="form_div"><form action="/comment/${id}" method="POST" class="comment-container"><label for = "usernamebox">Username</label><input name="commentUsername" type="text" class="usernamebox"/><label for = "commentbox">Comment here</label><input name="commentContent" type="textarea" class="commentbox"/><button type = "submit">Submit</button></form></div>`);
-            // $(`#${id} .comment`).on('click', function() {
-            //     $(`#${id} .form_div`).toggleClass(".form_div_show");
-            // });
-                    
-         
-
-            // $("#blogDisplay").append(`<div class="post_div" id=${id}></div>`);
-            // $(`#${id}`).append(`<div class="user_name"></div>`);
-            // $(`#${id} .user_name`).html(`${arrayUser}`);
-            // $(`#${id}`).append(`<div class="user_post"></div>`);
-            // $(`#${id} .user_post`).html(`${arrayPost}`);
-            // $(`#${id}`).append(`<button class="comment">Comment</button>`);
-            // $(`#${id} .comment`).on('click', function() {
-            //     $(`#${id} .post_section`).append(`<form action="/comment/${id}" method="POST" class="comment-container"><label for = "usernamebox">Username</label><input name="commentUsername" type="text" class="usernamebox"/><label for = "commentbox">Comment here</label><input name="commentContent" type="textarea" class="commentbox"/><button type = "submit">Submit</button></form>`);
-
-            }
+    }
 
 });
 
