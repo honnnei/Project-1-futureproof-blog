453f69aa16a8d6dd01f619ace8d1b69540e24fee
honnnei <hannpop@gmail.com>
2020-04-07 16:45:20 +0200

    comments display messed up

--- views/get-posts.js
+++ views/get-posts.js
@@ -41,11 +41,20 @@ $( document ).ready(function() {
             $(`#${id} .post_section`).append(`<div class="user_post"></div>`);
             $(`#${id} .post_section .user_post`).html(`${arrayPost}`);
             $(`#${id} .post_section`).append(`<button class="comment">Comment</button>`);
-            $(`#${id} .post_section .comment`).on('click', function() {
-                $(`#${id} .post_section .post_div`).append(`<form action="/comment/${id}" method="POST" class="comment-container"><label for = "usernamebox">Username</label><input name="commentUsername" type="text" class="usernamebox"/><label for = "commentbox">Comment here</label><input name="commentContent" type="textarea" class="commentbox"/><button type = "submit">Submit</button></form>`);
+            $(`#${id} .post_section `).append(`<form action="/comment/${id}" method="POST" class="comment-container"><label for = "usernamebox">Username</label><input name="commentUsername" type="text" class="usernamebox"/><label for = "commentbox">Comment here</label><input name="commentContent" type="textarea" class="commentbox"/><button type = "submit">Submit</button></form>`);
 
+            $(`#${id} .post_section .comment`).on('click', function() {
+                $(`#${id} .post_section form`).css("display", "flex");
             });
-            {
+                
+                
+                
+                
+                
+                // $(`#${id} .post_section .post_div`).append(`<form action="/comment/${id}" method="POST" class="comment-container"><label for = "usernamebox">Username</label><input name="commentUsername" type="text" class="usernamebox"/><label for = "commentbox">Comment here</label><input name="commentContent" type="textarea" class="commentbox"/><button type = "submit">Submit</button></form>`);
+        
+            // });
+            // {
             if (commentIndex === 0) { 
                 $(`#${id}`).append(`<div class="comment_section"></div>`);
                 $(`#${id} .comment_section`).append(`<div id=${commentId} class="comment_container"></div>`);
@@ -59,7 +68,7 @@ $( document ).ready(function() {
                 $(`#${commentId} .comment_username`).html(`${commentUser}`);
                 $(`#${commentId}`).append(`<div class="comment_content"></div>`);
                 $(`#${commentId} .comment_content`).html(`${commentValue}`);
-            }}
+            }
         }
 
         function displayPostNoComment(arrayIndex, arrayUser, arrayPost) {
