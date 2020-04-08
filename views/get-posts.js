5733fe4a197e09deb513b4841bf4d03b46c03c6e
honnnei <hannpop@gmail.com>
2020-04-08 15:56:12 +0200

    cleaned up the code

--- views/get-posts.js
+++ views/get-posts.js
@@ -28,18 +28,12 @@ $(document).ready(function () {
                 .then(imageUrl => {
 
                     if (!commentsPresent) {
-//emoji starts:
                         displayPostNoComment(index, postUser, postContent, postDate, imageUrl);
                         displayEmojiReacts(index);
                     } else {
                         let arrayOfComments = element.comments;
                         displayPostWithComment(index, postUser, postContent, arrayOfComments, element, postDate, imageUrl);
                         displayEmojiReacts(index);
-//this was on master before meoji:
-//                         displayPostNoComment(index, postUser, postContent, postDate, imageUrl);
-//                     } else {
-//                         let arrayOfComments = element.comments;
-//                         displayPostWithComment(index, postUser, postContent, arrayOfComments, element, postDate, imageUrl);
 
                     }
                 })                 //Had trouble with async. displayPost() must be here to ensure image is returned
@@ -50,18 +44,12 @@ $(document).ready(function () {
                 });
             } else { //calling display function if no giphy:
                 if (!commentsPresent) {
-//emoji starts:
                     displayPostNoComment(index, postUser, postContent, postDate);
                     displayEmojiReacts(index);
                 } else {
                     let arrayOfComments = element.comments;
                     displayPostWithComment(index, postUser, postContent, arrayOfComments, element, postDate);
                     displayEmojiReacts(index);
-//this was on master before meoji:
-//                     displayPostNoComment(index, postUser, postContent, postDate);
-//                 } else {
-//                     let arrayOfComments = element.comments;
-//                     displayPostWithComment(index, postUser, postContent, arrayOfComments, element, postDate);
                 }
             }
 
@@ -69,11 +57,7 @@ $(document).ready(function () {
         }); //array map close
     }); //axios close
 
-//emoji starts:
     function displayPostWithComment(arrayIndex, arrayUser, arrayPost, commentArray, postElement, arrayDate, arrayImage ) {
-//this was on master before meoji:
-//     function displayPostWithComment(arrayIndex, arrayUser, arrayPost, commentArray, postElement, arrayDate, arrayImage) {
-
         //taken out of the function arguments: commentIndex, commentUser, commentValue, commentNumber
         let id = arrayIndex.toString();
         //displaying the post:
@@ -135,11 +119,8 @@ $(document).ready(function () {
         $(`#${commentId} .comment_content`).html(`${contentOfComment}`);   
     }
 
-//emoji starts
-    function displayPostNoComment(arrayIndex, arrayUser, arrayPost, arrayDate, arrayImage) {
-//before meoji
-//     function displayPostNoComment(arrayIndex, arrayUser, arrayPost, arrayDate, arrayImage) {
 
+    function displayPostNoComment(arrayIndex, arrayUser, arrayPost, arrayDate, arrayImage) {
         let id = arrayIndex.toString();
         console.log(id);
 
@@ -181,49 +162,6 @@ $(document).ready(function () {
     }
 
 });
-  
-// function makeAGiphy(query) {
-//             let imageSource = "";
-//                 let key = "nNH7998Ir4ao8g1OTRrIQxqYr1EomuJO"
-//                 let url = `https://api.giphy.com/v1/gifs/search?api_key=${key}&q=${query}&limit=1&offset=0&rating=G&lang=en` //API call. 1 image is returned for user's search query
-
-//                 fetch(url) //Returns image url response from giphy api
-//                 .then(response => response.json())
-//                 .then(content => {
-//                     let image = content.data[0].id;
-//                     imageSource = "https://media.giphy.com/media/" + image + "/giphy.gif";
-//                     console.log(imageSource);
-//                     // displayPost(index, postUser, postContent, image);                   //Had trouble with async. displayPost() must be here to ensure image is returned
-//                     return imageSource;
-//                     displayPostNoComment();
-//                     displayPostWithComment();
-//                 })
-//                 .catch(err=>{
-//                     console.error(err);
-//                 })
-           
-//     }
-     
-function displayPost(arrayIndex, arrayUser, arrayPost, arrayDate, arrayImage ) {
-    let id = arrayIndex.toString();
-    console.log(id);
-    //console.log(arrayImage);
-    $("#blogDisplay").append(`<div class="post_div" id=${id}></div>`);
-    // $(`#${id}`).append(`<div class="user-icon"><p>&#129409;</p></div>`);
-    $(`#${id}`).append(`<div class="post_content"></div>`);
-    $(`#${id} .post_content`).append(`<div class="user_name"></div>`);
-    $(`#${id} .post_content`).append(`<div class="user_post_image"></div>`);
-    $(`#${id} .user_name`).html(`&#129409 ${arrayUser}`);
-    $(`#${id} .user_post_image`).append(`<div class="user_post"></div>`);
-    $(`#${id} .user_post`).html(`${arrayPost}`);
-    if(arrayImage){
-        $(`#${id} .user_post_image`).append(`<div class="user_image"></div>`);
-        $(`#${id} .user_image`).html(`<img class ="giphyImage" src="${arrayImage}">`); //Applied class "giphyImage" to reduce image size. Feel free to change styling.
-    }
-    $(`#${id}`).append(`<button class="comment">Comment</button>`);
-
-//     $(`#${id} .post_content`).append(`<button class="comment">Comment</button>`); - from matt-style
-}
 
 function displayEmojiReacts(index) {
     let id = index.toString();
